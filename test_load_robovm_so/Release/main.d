7963e1fb72ecf8ccabb512d8f4c2b0bf2ff74750
Chris Fogelklou <chris.fogelklou@gmail.com>
2015-06-12 02:48:26 +0200

    Add linux build files.

---
+++ test_load_robovm_so/Release/main.d
@@ -0,0 +1,7 @@
+main.d: ../main.c \
+ /home/appsupport/Projects/robovm/robovm/vm/core/include/jni.h \
+ /home/appsupport/Projects/robovm/robovm/vm/core/include/jni_types.h
+
+/home/appsupport/Projects/robovm/robovm/vm/core/include/jni.h:
+
+/home/appsupport/Projects/robovm/robovm/vm/core/include/jni_types.h:
