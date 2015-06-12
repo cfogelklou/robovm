d6a5766e87786773feb3d07d4734e45f42a26f39
Chris Fogelklou <chris.fogelklou@gmail.com>
2015-06-12 03:07:52 +0200

    Update so buildlib builds both exe and lib Fix buildexe.

--- test_load_robovm_so/ubuntu_buildexe.sh
+++ test_load_robovm_so/ubuntu_buildexe.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 export ROBOVM_DEV_ROOT=/home/appsupport/Projects/robovm/robovm
 cd $ROBOVM_DEV_ROOT
+# Build the runtime libraries
 vm/build.sh
 vm/copy-binaries.sh
+
 cd test_load_robovm_so
 rm -rf src_exe/classes
 mkdir src_exe/classes
 javac -d src_exe/classes/ src_exe/HelloWorld.java
-$ROBOVM_DEV_ROOT/bin/robovm -verbose -dump-intermediates -os linux -binaryType executable -forcelinkclasses HelloWorld -skiprt -bcp $ROBOVM_DEV_ROOT/rt/target/classes:/robovm-rt/target/test-classes -cp $ROBOVM_DEV_ROOT/test_load_robovm_so/src_exe/classes HelloWorld
-./src_exe/HelloWorld/HelloWorld -rvm:log=debug
+$ROBOVM_DEV_ROOT/bin/robovm -dump-intermediates -os linux -binaryType executable -forcelinkclasses HelloWorld -skiprt -bcp $ROBOVM_DEV_ROOT/rt/target/classes:/robovm-rt/target/test-classes -cp $ROBOVM_DEV_ROOT/test_load_robovm_so/src_exe/classes HelloWorld
+
+./HelloWorld/HelloWorld -rvm:log=debug
 
 
--- test_load_robovm_so/ubuntu_buildlib.sh
+++ test_load_robovm_so/ubuntu_buildlib.sh
@@ -11,17 +11,27 @@ cd Debug
 make clean all
 cd ..
 
-#Build the test library that the test program will load.
+#Delete the executable and the library.
 rm -rf src_lib/classes
 mkdir src_lib/classes
+rm -rf src_exe/classes
+mkdir src_exe/classes
 
 # Compile the test library
 javac -d src_lib/classes/ src_lib/TestLib.java
 
+# Compile HelloWorld
+javac -d src_exe/classes/ src_exe/HelloWorld.java
+
 #Use our debug compiler to build the test library.
 $ROBOVM_DEV_ROOT/bin/robovm -verbose -dump-intermediates -os linux -binaryType dynamic_lib -forcelinkclasses TestLib -skiprt -bcp $ROBOVM_DEV_ROOT/rt/target/classes:/robovm-rt/target/test-classes -cp $ROBOVM_DEV_ROOT/test_load_robovm_so/src_lib/classes TestLib 
+
+#Use our debug compiler to build the HelloWorld application.
+$ROBOVM_DEV_ROOT/bin/robovm -verbose -dump-intermediates -os linux -binaryType executable -forcelinkclasses HelloWorld -skiprt -bcp $ROBOVM_DEV_ROOT/rt/target/classes:/robovm-rt/target/test-classes -cp $ROBOVM_DEV_ROOT/test_load_robovm_so/src_exe/HelloWorld/classes HelloWorld
 
 #Run the  test library.
 ./Debug/test_load_robovm_so
 
+#Run HelloWorld.
+./src_exe/HelloWorld/HelloWorld -rvm:log=debug
 
