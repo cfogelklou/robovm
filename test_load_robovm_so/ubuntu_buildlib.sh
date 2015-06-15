768e458bb6a2f39a7465fde0f2c8c7581d9d8a09
Chris Fogelklou <chris.fogelklou@gmail.com>
2015-06-15 08:03:45 +0200

    Update so java is invoked to run the app, instead of trying to run the compiler via the robovm script (since it doesn't work)

--- test_load_robovm_so/ubuntu_buildlib.sh
+++ test_load_robovm_so/ubuntu_buildlib.sh
@@ -25,11 +25,14 @@ javac -d src_lib/classes/ src_lib/TestLib.java
 javac -d src_exe/classes/ src_exe/HelloWorld.java
 
 #Use our debug compiler to build the test library.
-$ROBOVM_DEV_ROOT/bin/robovm -dump-intermediates -os linux -binaryType dynamic_lib -forcelinkclasses TestLib -skiprt -bcp $ROBOVM_DEV_ROOT/rt/target/classes:/robovm-rt/target/test-classes -cp $ROBOVM_DEV_ROOT/test_load_robovm_so/src_lib/classes TestLib 
+cd $ROBOVM_DEV_ROOT/compiler
+java -cp $ROBOVM_DEV_ROOT/compiler/target/robovm-compiler-1.3.1-SNAPSHOT.jar org.robovm.compiler.AppCompiler -dump-intermediates -os linux -binaryType dynamic_lib -forcelinkclasses TestLib -skiprt -bcp $ROBOVM_DEV_ROOT/rt/target/classes:$ROBOVM_DEV_ROOT/robovm-rt/target/test-classes -cp $ROBOVM_DEV_ROOT/test_load_robovm_so/src_lib/classes TestLib
 
 #Use our debug compiler to build the HelloWorld application.
-$ROBOVM_DEV_ROOT/bin/robovm -dump-intermediates -os linux -binaryType executable -forcelinkclasses HelloWorld -skiprt -bcp $ROBOVM_DEV_ROOT/rt/target/classes:/robovm-rt/target/test-classes -cp $ROBOVM_DEV_ROOT/test_load_robovm_so/src_exe/classes HelloWorld
+cd $ROBOVM_DEV_ROOT/compiler
+java -cp $ROBOVM_DEV_ROOT/compiler/target/robovm-compiler-1.3.1-SNAPSHOT.jar org.robovm.compiler.AppCompiler -dump-intermediates -os linux -binaryType executable -forcelinkclasses HelloWorld -skiprt -bcp $ROBOVM_DEV_ROOT/rt/target/classes:$ROBOVM_DEV_ROOT/robovm-rt/target/test-classes -cp $ROBOVM_DEV_ROOT/test_load_robovm_so/src_exe/classes HelloWorld
 
+cd $ROBOVM_DEV_ROOT/test_load_robovm_so
 #Run the  test library.
 ./Debug/test_load_robovm_so
 
